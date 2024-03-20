6db671d2f039be568f0061686a251bd5648797ec
Joel Bowman <jbowwww@gmail.com>
2024-03-20 20:31:36 +0800

    Correct mongo DB type in db.runCommand

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,13 @@
 import mongo from 'mongodb';
 
-export async function runCommand(url: string, options: mongo.MongoClientOptions = {}, command: (db: mongo.MongoClient) => Promise<void>) {
-    const client = new mongo.MongoClient(url, options);
-    const db = await client.connect();
+export async function runCommand(url: string, options: mongo.MongoClientOptions = {}, command: (db: mongo.Db) => Promise<void>) {
+    const client = await mongo.MongoClient.connect(url, options);
+    const db = client.db();
     try {
         command(db);
     } catch (err) {
         throw err;
     } finally {
-        await db.close();
+        await client.close();
     }
 }
