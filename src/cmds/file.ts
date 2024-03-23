e1845822f751d20b9facc868478e14ad3fdc107d
Joel Bowman <jbowwww@gmail.com>
2024-03-23 19:45:43 +0800

    Added missing $updateOne operator on mongo updateOne()

--- src/cmds/file.ts
+++ src/cmds/file.ts
@@ -5,7 +5,7 @@ import { calculateHash } from "../file";
 
 export interface FileCommandArgv {
     dbUrl: string,
-    path: string,
+    paths: string[],
 }
 
 export interface File {
@@ -22,40 +22,42 @@ export interface File {
 exports.command = 'file';
 exports.description = 'File commands';
 exports.builder = function (yargs: yargs.Argv<FileCommandArgv>) {
-    yargs.command('index <path>', 'Index file', yargs => {
-        yargs.positional('path', {
-            description: 'Path to file',
-            type: 'string',
+    yargs.command('index <paths...>', 'Index file', yargs => {
+        yargs.positional('paths', {
+            description: 'Path(s) to file(s) or shell glob expression(s) that will get expanded',
+            array: true,
             demandOption: true
         });
     }, async function (argv) {
-        const path = argv.path;
-        const stats = await fs.promises.stat(path);
-        if (!stats.isFile())
-            throw new Error(`Path '${path}' is not a file`);
-        else
-            console.log(`File '${path}' stat=${JSON.stringify(stats)}`);
-        await db.runCommand(argv.dbUrl, {}, async db => {
-            const coll = db.collection<File>('local');
-            const file = await coll.findOne({ path: path });
-            let doHash = true;
-            if (file) {
-                if (file.stats.size == stats.size || file.stats.mtimeMs == stats.mtimeMs) {
-                    console.log(`File '${path}' matches local DB: ${JSON.stringify(file)}`);
-                    doHash = false;
+        for (const path of argv.paths) {
+            const stats = await fs.promises.stat(path);
+            if (!stats.isFile())
+                throw new Error(`Path '${path}' is not a file`);
+            else
+                process.stdout.write(`File '${path}'`);
+            await db.runCommand(argv.dbUrl, {}, async db => {
+                const coll = db.collection<File>('local');
+                const file = await coll.findOne({ path: path });
+                let doHash = true;
+                if (file) {
+                    if (file.stats.size == stats.size || file.stats.mtimeMs == stats.mtimeMs) {
+                        console.log(`File '${path}' matches local DB: ${JSON.stringify(file)}`);
+                        doHash = false;
+                    } else {
+                        console.log(`File '${path}' does not match local DB: ${JSON.stringify(file)}\n\tFile.stat=${JSON.stringify(stats)}`);
+                    }
                 } else {
-                    console.log(`File '${path}' does not match local DB: ${JSON.stringify(file)}`);
+                    console.log(`File '${path}' not present in local DB stat=${JSON.stringify(stats)}`);
                 }
-            } else {
-                console.log(`File '${path}' not present in local DB`);
-            }
-            if (doHash) {
-                process.stdout.write(`Calculating hash for file '${path}' ... `)
-                const hash = await calculateHash(path);
-                await coll.updateOne({ path }, { path, stats, hash }, { upsert: true });
-                console.log(hash);
-            }
-        });
+                if (doHash) {
+                    process.stdout.write(`Calculating hash for file '${path}' ... `)
+                    const hash = await calculateHash(path);
+                    await coll.updateOne({ path }, { $updateOne: { path, stats, hash } }, { upsert: true });
+                    console.log(hash);
+                }
+                console.log();
+            });
+        }
     })
     yargs.demandCommand();
 };
