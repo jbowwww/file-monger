e075268e2d7eed966015258e8873095abf045444
Joel Bowman <jbowwww@gmail.com>
2024-03-23 19:33:35 +0800

    Removed input stream.on('close') handler

--- src/file.ts
+++ src/file.ts
@@ -6,7 +6,6 @@ export async function calculateHash(path: string) {
         const hashDigest = crypto.createHash('sha256');
         const input = fs.createReadStream(path);
         const hash = await new Promise((resolve: (value: string) => void, reject): void => {
-            input.on('close', () => resolve(hashDigest.digest('hex')));
             input.on('end', () => resolve(hashDigest.digest('hex')));
             input.on('error', () => reject(`Error hashing file '${path}'`));
             input.on('readable', () => {
