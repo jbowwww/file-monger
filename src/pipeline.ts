09e9a210960cc3358f274bb5fedd2877b41bcfe6
Joel Bowman <jbowwww@gmail.com>
2025-02-08 07:54:09 +0800

    Change pipeline functions (currently unused) to operate in terms of generators not xform functions

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -6,7 +6,7 @@ export type AsyncGeneratorFunction<I = any, O = any> = (source: AsyncIterable<I>
 export type PipelineFunctionStage<I = any, O = any> = (input: I) => O | Promise<O>;
 export type PipelineGeneratorStage<I = any, O = any> = (source: AsyncGenerator<I>) => AsyncGenerator<O>;
 export type PipelineStage<I = any, O = any> = PipelineFunctionStage<I, O> | PipelineGeneratorStage<I, O>;
-export type Pipeline<I = any, O = any> = PipelineFunctionStage<I, O>;
+export type Pipeline<I = any, O = any> = PipelineGeneratorStage<I, O>;
 
 // export type Pipeline<I = any, O = any> = (source: AsyncGenerator<I>) => AsyncGenerator<O> & {
 //     run(source: AsyncGenerator<I>): AsyncGenerator<O>;
@@ -22,26 +22,28 @@ export const makeGeneratorFnFromFn = <I = any, O = any>(stage: PipelineFunctionS
 export const isAsyncGenerator = (generator: any): generator is AsyncGenerator => (["next", "return", "throw", Symbol.asyncIterator]).every(prop => typeof prop === "function");
 export const isAsyncGeneratorFunction = (generatorFn: any): generatorFn is AsyncGeneratorFunction => isAsyncGenerator(generatorFn.prototype);
 
-export const compose = <I = any, O = any>(...stages: PipelineFunctionStage[]) => stages.reduce(
-    (prevStages, stage, i, arr) => async (input: any) => stage(prevStages(await input)) as O | Promise<O>) as Pipeline<I, O>;
+// export const compose = <I = any, O = any>(...stages: PipelineFunctionStage[]) => stages.reduce(
+//     (prevStages, stage, i, arr) => async (input: any) => stage(prevStages(await input)) as O | Promise<O>) as Pipeline<I/* , O>;
+export const compose = <I = any, O = any>(...stages: PipelineGeneratorStage<any>[]) => (source: AsyncGenerator<I>) =>
+    stages.reduce<AsyncGenerator<any>>((prevStages, stage, i, arr) => stage(prevStages), source) as AsyncGenerator<O> /* as Pipeline<I, O> */;
 
-export const pipeline = <I = any, O = any>(...stages: PipelineFunctionStage[]) => {
+export const pipeline = compose;/*  <I = any, O = any>(...stages: PipelineFunctionStage[]) => {
     const transform = compose<I, O>(...stages);
     return async function* (source: AsyncIterable<I>) {
         for await (const input of source) {
             yield await transform(input);
         }
     };
-};
+}; */
 
-export const pipe = <I = any, O = any>(source: AsyncIterable<I>, ...stages: PipelineFunctionStage[]) => pipeline(...stages)(source);
+export const pipe = <I = any, O = any>(source: AsyncGenerator<I>, ...stages: PipelineFunctionStage[]) => pipeline(...stages)(source);
 
-export const run =
-    async function* <I = any, O = any>(source: AsyncIterable<I>, pipeline: Pipeline<I, O>): AsyncIterable<O> {
-        for await (const input of source) {
-            yield await pipeline(input);
-        }
-    };
+// export const run =
+//     async function* <I = any, O = any>(source: AsyncIterable<I>, pipeline: Pipeline<I, O>): AsyncIterable<O> {
+//         for await (const input of source) {
+//             yield await pipeline(input);
+//         }
+//     };
 
 export const iff = <I = any, O = any>(condition: (input: I) => boolean | Promise<boolean>, stage: PipelineFunctionStage<I, O>) =>
     async (input: I) => (await condition(input) ? await (stage as PipelineFunctionStage<I, O>)(input) : input);
