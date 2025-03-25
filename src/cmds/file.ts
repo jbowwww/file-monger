780880cf167d8c596993f440d2c98162ac9b94d6
Joel Bowman <jbowwww@gmail.com>
2025-03-25 22:39:51 +0800

    Fixed FileSystemArtefactSchema typing. Updated handleError(). Removed cruft.

--- src/cmds/file.ts
+++ src/cmds/file.ts
@@ -10,46 +10,11 @@ import { AnyBulkWriteOperation, MongoError } from "mongodb";
 
 import debug from "debug";
 import { Artefact } from "../models/artefact";
+import { DiscriminatedModel } from "../models";
 const log = debug(nodePath.basename(module.filename));
 
-export type FileSystemSchema = /* Artefact< */{
-    // get Entry(): Entry { return this.File ?? this.Directory ?? this.Unknown!; }
-    File?: File;
-    Directory?: Directory;
-    Unknown?: Unknown;
-    Hash?: Hash;
-    Partition?: Partition;
-}/* > */;
+export type FileSystemSchema = DiscriminatedModel<File | Directory | Unknown | Hash | Partition | Disk>;
 export type FileSystemArtefactSchema = Artefact<FileSystemSchema>;
-//     constructor(data: Partial<FileSystemArtefactSchema> | Entry) {
-//         if (isAspect<Entry>(data)) {
-//             super();
-//             log("FileSystemArtefact.ctor(): isAspect(data)=true: data=%O this=%O", data, this);
-//             this[data._T as string] = data;
-//         } else {
-//             const _data = data as Partial<FileSystemArtefactSchema>;
-//             super({ _id: _data._id, _e: _data._e });
-//             this.File = _data.File;
-//             this.Directory = _data.Directory;
-//             this.Unknown = _data.Unknown;
-//             log("FileSystemArtefact.ctor(): isAspect(data)=false: _data=%O this=%O", _data, this);
-//         }
-//     }
-    
-//     // Queries defined in this static member also get copied to the instance prototype, currying the parameter with this
-//     // So if your instance variable is _, queries will be available like e.g. _.Query.byPath() and it will use the path value of this instance
-//     static Query = {
-//         ...Artefact.Query,
-//         byUnique: (_: Artefact) => this.Query.byIdOrPath(_ as FileSystemArtefactSchema) as Filter<Artefact>,
-//         byPath: (_: FileSystemArtefactSchema) => ({ [_.Entry._T + ".path"]: { $eq: _.Entry.path } }) as Filter<FileSystemArtefactSchema>,
-//             // _.File       ? { "File.path":        { $eq: _.File!.path }      } :
-//             // _.Directory  ? { "Directory.path" :  { $eq: _.Directory!.path } } :
-//             // _.Unknown    ? { "Unknown.path":     { $eq: _.Unknown!.path   } } :
-//             // {}
-//         // ) as Filter<FileSystemArtefact>,
-//         byIdOrPath: (_: FileSystemArtefactSchema) => _._id ? this.Query.byId(_) : this.Query.byPath(_) as Filter<FileSystemArtefactSchema>,
-//     };
-// }
 
 export const command = 'file';
 export const description = 'File commands';
@@ -67,7 +32,7 @@ export const builder = (yargs: yargs.Argv) => yargs
         async function (argv): Promise<void> {
 
             const storage = new MongoStorage(argv.dbUrl);
-            const store = await storage.store<FileSystemSchema>("fileSystemEntries", {
+            const store = await storage.store<FileSystemArtefactSchema>("fileSystemEntries", {
                 createIndexes: [{
                     index: { "File.path": 1, "Directory.path": 1, "Unknown.path": 1, "Partition.uuid": 1, "Disk.model": 1, "Disk.serial": 1, },
                     options: { unique: true, },
@@ -88,7 +53,7 @@ export const builder = (yargs: yargs.Argv) => yargs
                     await Task.repeat({ postDelay: 5000 }, async() => {           // 5s
                         const disks = await getDisks();
                         const partitions = await getPartitions();
-                        const ops: AnyBulkWriteOperation<Artefact<FileSystemArtefactSchema>>[] = [];
+                        const ops: AnyBulkWriteOperation<FileSystemArtefactSchema>[] = [];
                         ops.push(...disks.map(d => ({ "updateOne": {
                             filter: { $and: [ { "Disk.model": { $eq: d.model } }, { "Disk.serial": { $eq: d.serial } }, ], },
                             update: { $set: { "Disk": d } },
@@ -110,7 +75,7 @@ export const builder = (yargs: yargs.Argv) => yargs
                                 const _ = { [fsEntry._T]: fsEntry, };
                                 try {
                                     log("%s: _=%O", task.name, _);
-                                    const result = await store.updateOrCreate(_, { [`${fsEntry._T}.path`]: fsEntry.path });// Query<FileSystemArtefactSchema>(`${fsEntry._T}.path`, fsEntry.path);
+                                    const result = await store.updateOrCreate(_, { [`${fsEntry._T}.path`]: fsEntry.path });
                                     log("%s: result=%O\n%s: task.progress=%s", task.name, result, task.name, task.progress);
                                 } catch (e: any) {
                                     handleError(e, task, _, store);
@@ -172,10 +137,18 @@ export const builder = (yargs: yargs.Argv) => yargs
         }
     ).demandCommand();
 
-async function handleError(e: Error, task: Task, _: Partial<FileSystemArtefactSchema>, store: Store<FileSystemSchema>) {
+async function handleError(e: Error, task: Task, _: Partial<FileSystemArtefactSchema>, store: Store<FileSystemArtefactSchema>) {
     // const error = Object.assign(e, { task: task });//new Error("${task.name}: Error!\n_=${nodeUtil.inspect(_, false, 1)}\nError={e/* .stack */}"), { /* cause: e, stack: e.stack */ });
     if (!(e instanceof MongoError)) {
-        const result = await store.updateOne(_._id ? { _id: _._id } : _.File ? { "File.path": _.File.path } : _.Directory ? { "Directory.path": _.Directory.path } : { "Unknown.path": _.Unknown?.path }, { $set: { _e: [e] } });
+        const result = await store.updateOne(
+            _._id ? { _id: _._id } :
+            _.File ? { "File.path": _.File.path } :
+            _.Directory ? { "Directory.path": _.Directory.path } :
+            _.Unknown ? { "Unknown.path": _.Unknown?.path } :
+            _.Partition ? { "Partition.uuid": _.Partition?.uuid } :
+            _.Disk ? { $and: [ { "Disk.model": _.Disk.model }, { "Disk.serial": _.Disk?.serial }, ], } :
+            {},
+            { $set: { _e: [e] } });
         task.warnings.push(e);
         log("${task.name}: Warn! _=%O Error=%s", _, e.stack);
     } else {
