73fc89f0267c4472e0af83a6b37aae36a327b16a
Joel Bowman <jbowwww@gmail.com>
2025-06-07 09:34:33 +0800

    Cleanup

--- src/cli.ts
+++ src/cli.ts
@@ -11,15 +11,14 @@ export const globalOptions = {
     dbUrl: {
         description: "Path to database",
         demandOption: true,
-        default: "mongodb://mongo:mongo@127.0.0.1:27017/test?directConnection=true&replicaSet=rs0&authSource=admin", // const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongo-database/${process.env.DB_NAME}?retryWrites=true&writeConcern=majority&authSource=admin`;
+        default: "mongodb://mongo:mongo@127.0.0.1:27017/test?directConnection=true&replicaSet=rs0&authSource=admin",
         global: true,
     }
-};
+}
 
 var argv = yargs(hideBin(process.argv))
     .scriptName("cli")
     .option(globalOptions)
-    .middleware(async argv => { log("cli middleware argv=%O", argv); })
     .commandDir("cmds")
     .demandCommand()
     .help("help", "Help", true)
